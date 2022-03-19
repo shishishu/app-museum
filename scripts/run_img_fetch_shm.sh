5f72aa76d99f56ef9b7d08ca7baa330997fdf7c5
shishishu <zhouxj_xinhua@163.com>
2022-03-19 20:36:26 +0800

    opt

--- scripts/run_img_fetch_shm.sh
+++ scripts/run_img_fetch_shm.sh
@@ -6,14 +6,8 @@ curr_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 root_dir=$(dirname $curr_dir)
 
 worker_num=20
-for ((i=0; i<10; i++))
+for ((i=0; i<${worker_num}; i++))
 do
-    python ${root_dir}/img_fetch_shm.py ${worker_num} ${i} > ${root_dir}/log/shm_4th_${i}.log 2>&1 &
+    python ${root_dir}/img_fetch_shm.py ${worker_num} ${i} > ${root_dir}/log/shm_4th_${i}.log 2>&1
 done 
 
-sleep 2h
-
-for ((i=10; i<${worker_num}; i++))
-do
-    python ${root_dir}/img_fetch_shm.py ${worker_num} ${i} > ${root_dir}/log/shm_4th_${i}.log 2>&1 &
-done 
